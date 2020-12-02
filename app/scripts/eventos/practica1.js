2a0093f03e3dd9c7e211932540039e60b7774b5e
Cristian <cristian.asun.mor@gmail.com>
2020-12-02 14:34:53 +0100

    ev_pr1

---
+++ app/scripts/eventos/practica1.js
@@ -0,0 +1,5 @@
+document.addEventListener('keyup', (event) => {
+    if(event.key == 'F12' && event.altKey){
+        document.body.style.backgroundImage = "url('./images/eventos_p1_bg.jpg')";
+    }
+});
